07d77663083145d5ca4e262bded3114ec875b611
marcocarneiro <marco.carneiro7@gmail.com>
2024-03-28 16:07:25 -0300

    Testes com DataGrid

--- src/components/DataGrid.tsx
+++ src/components/DataGrid.tsx
@@ -1,17 +1,40 @@
-/* import CssBaseline from "@mui/material/CssBaseline"
-import GlobalStyles from "@mui/material/GlobalStyles" 
-import { TextCenter } from "../components/helpers/estilos"*/
 import { Refine } from "@refinedev/core";
 import dataProvider from "@refinedev/simple-rest";
+import { useModalForm } from "@refinedev/react-hook-form";
+import { Modal } from "@mui/material";
+
 import { ProductTable } from "../components/base/BasicDataGrid"
 
 const API_URL = "http://localhost:8800"
  
 const DataGrid = () => {
+
     return(
-        <Refine dataProvider={dataProvider(API_URL)}>
-            <ProductTable />
-        </Refine>  
+        <>
+            
+            <Refine
+                dataProvider={dataProvider(API_URL)}
+                resources={[
+                    {
+                        name: "books",
+                        list: "/books",
+                        create: "/insertbook",
+                        edit: "/updatebook/:id",
+                        meta: {
+                            canDelete: true,
+                        },
+                    },
+                ]}
+                options={{
+                    syncWithLocation: true,
+                    warnWhenUnsavedChanges: true,
+                    useNewQueryKeys: true,
+                    projectId: "ccm8pQ-GWknW0-1wHNof",
+                }}
+                >
+                <ProductTable />
+            </Refine>
+        </>        
     )    
 }
 export default DataGrid
--- src/components/base/BasicDataGrid.tsx
+++ src/components/base/BasicDataGrid.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useDataGrid } from "@refinedev/mui";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { EditButton, DeleteButton } from "@refinedev/mui";
 
 export const ProductTable: React.FC = () => {
     const { dataGridProps } = useDataGrid<IProduct>({
         resource: "books",
     });
+
+    const alerta = ()=>{alert('Clicou!!')}
 
     const columns = React.useMemo<GridColDef<IProduct>[]>(
         () => [
@@ -19,13 +22,26 @@ export const ProductTable: React.FC = () => {
             { field: "desc", headerName: "Descrição", minWidth: 400, flex: 1 },
             { field: "price", headerName: "Price", minWidth: 120, flex: 0.3 },
             { field: "cover", headerName: "Capa", minWidth: 120, flex: 0.3 },
+            {
+                field: "actions",
+                headerName: "Actions",
+                sortable: false,
+                renderCell: function render({ row }) {
+                  return (
+                    <>
+                      <EditButton hideText recordItemId={row.id}  />
+                      <DeleteButton hideText recordItemId={row.id} />
+                    </>
+                  );
+                },
+            },            
         ],
         [],
     );
 
     return (
         <div style={{ padding:"4px" }}>
-            <h2>Products</h2>
+            <h2>Livros</h2>
             <DataGrid {...dataGridProps} columns={columns} />
         </div>
     );
